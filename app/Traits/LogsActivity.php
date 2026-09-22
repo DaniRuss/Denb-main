daab184ad761e308fb66f1ca881658be1c5b127e
fuad-266 <fuadabdala266@gmail.com>
2026-09-22 17:13:59 +0300

    add database connectivity test script for local development environment

---
+++ app/Traits/LogsActivity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Traits;
+
+trait LogsActivity
+{
+    /**
+     * Boot the LogsActivity trait for a model.
+     */
+    protected static function bootLogsActivity(): void
+    {
+        // TODO: Implement logging logic (e.g., created, updated, deleted events).
+    }
+}
